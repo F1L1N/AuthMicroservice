786ae3c1765ce326cc235410e569e5a3badf6543
pavel_filin <pavel.filin.rm@gmail.com>
2020-03-19 00:51:23 +0300

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthMicroservice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
